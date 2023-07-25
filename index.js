8b68d306fb588309c99403673c226835f9dc2a11
leBryanXD <111110243+leBryanXD@users.noreply.github.com>
2023-07-24 20:07:05 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+document.querySelector(".nav_menu").addEventListener("click", () => {
+  document.querySelector(".content_nav").classList.toggle("active");
+});
+
+const carrusel = document.querySelector(".carrusel-items");
+
+let maxScrollLeft = carrusel.scrollWidth - carrusel.clientWidth;
+let intervalo = null;
+let step = 1;
+const start = () => {
+  intervalo = setInterval(function () {
+    carrusel.scrollLeft = carrusel.scrollLeft + step;
+    if (carrusel.scrollLeft === maxScrollLeft) {
+      step = step * -1;
+    } else if (carrusel.scrollLeft === 0) {
+      step = step * -1;
+    }
+  }, 10);
+};
+
+const stop = () => {
+  clearInterval(intervalo);
+};
+
+carrusel.addEventListener("mouseover", () => {
+  stop();
+});
+
+carrusel.addEventListener("mouseout", () => {
+  start();
+});
+
+start();
